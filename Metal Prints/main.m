c86cd3587b0f6d5fc0f4616abed49b7192290eb3
stevens-apps <stevens_apps@yahoo.com>
2016-04-20 21:17:26 -0700

    Initial Commit

---
+++ Metal Prints/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Metal Prints
+//
+//  Created by Bryan Stevens on 4/20/16.
+//  Copyright © 2016 Pocket Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
